9868967bd67cc0e6c947084a4729acf23910dcc2
SARAVANAN <sb9489.rs@gmail.com>
2023-05-31 00:23:44 +0530

    last

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -4,84 +4,82 @@ const Employees = require('../models/employees.model');
 const router = express.Router();
 
 router.get('/employees', (req, res) => {
-   
-    try{
+
+    try {
         Employees.find((err, data) => {
-            if(err){
-                return res.status(400).send({message: 'Error while retrieving employees. Please check the data'})
+            if (err) {
+                return res.status(400).send({ message: 'Error while retrieving employees. Please check the data' })
             }
 
             res.status(200).send(data);
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).send({
             message: 'Internal Server Error'
         })
     }
 });
 
+router.get('/employees/:empID', (req, res) => {
+    try {
+        Employees.findOne({ _id: req.params.empID }, (err, data) => {
+            if (err) {
+                return res.status(400).send({ message: 'Error while retrieving an employee. Please check the data' })
+            }
+
+            res.status(200).send(data);
+        })
+    } catch (error) {
+        res.status(500).send({
+            message: 'Internal Server Error'
+        })
+    }
+});
 
 router.post('/employees', (req, res) => {
     try {
-        const payload= req.body;
+        const payload = req.body;
 
-        const newEmployee= new Employees(payload);
-         newEmployee.save((err,data)=>{
-            if(err){
-                return res.status(400).send({message: 'error while adding new employee. please check the data'})
+        const newEmployee = new Employees(payload);
+        newEmployee.save((err, data) => {
+            if (err) {
+                return res.status(400).send({ message: 'error while adding new employee. please check the data' })
             }
-            res.status(200).send({employeeId: data._id,message: "Employee has been added successfully."})
-         })
+            res.status(200).send({ employeeId: data._id, message: "Employee has been added successfully." })
+        })
     } catch (error) {
-        
+
     }
 
 });
-router.get('/employees/:empID', (req, res) => {
-    try{
-        Employees.findOne({_id: req.params.empID}, (err, data) => {
-            if(err){
-                return res.status(400).send({message: 'Error while retrieving an employee. Please check the data'})
-            }
-
-            res.status(200).send(data);
-        })
-    }catch(error){
-        res.status(500).send({
-            message: 'Internal Server Error'
-        })
-    }
-});
-
-
 
 router.put('/employees/:empID', (req, res) => {
-    try{
-        Employees.findByIdAndUpdate({_id: req.params.empID}, {$set: req.body}, (err, data) =>{
-            if(err){
-                return res.status(400).send({message: 'Error while updating an existing user. Please check the data'})
+    try {
+        Employees.findByIdAndUpdate({ _id: req.params.empID }, { $set: req.body }, (err, data) => {
+            if (err) {
+                return res.status(400).send({ message: 'Error while updating an existing user. Please check the data' })
             }
 
-            res.status(201).send({employeeId: data._id, message: "Employee details have been updated."})
+            res.status(201).send({ employeeId: data._id, message: "Employee details have been updated." })
         })
 
-    }catch(error){
+    } catch (error) {
         res.status(500).send({
             message: "Internal Server Error"
         })
     }
 });
 router.delete('/employees/:empID', (req, res) => {
-  
-    try{
-        Employees.deleteOne({_id: req.params.empID}, (err, data) => {
-            if(err){
+
+    try {
+        Employees.deleteOne({ _id: req.params.empID }, (err, data) => {
+            if (err) {
                 return res.status(400).send('Error while deleting an employee. Please check the data');
             }
 
-            res.status(200).send({message : `Employee with id ${req.params.empID} has been deleted.`})
+            res.status(200).send({ message: `Employee with id ${req.params.empID} has been deleted.` })
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).send({
             message: "Internal Server Error"
         })
@@ -90,4 +88,4 @@ router.delete('/employees/:empID', (req, res) => {
 
 
 
-module.exports = router ;
+module.exports = router;
